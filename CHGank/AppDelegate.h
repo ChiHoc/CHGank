2f74a8352c85be6e8799fad72bf8053c34e55ca2
ChiHo <genioschen@gmail.com>
2016-05-23 00:00:13 +0800

    Init

---
+++ CHGank/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHGank
+//
+//  Created by ChiHo on 5/22/16.
+//  Copyright © 2016 ChiHo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CHGank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CHGank
+//
+//  Created by ChiHo on 5/22/16.
+//  Copyright © 2016 ChiHo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
